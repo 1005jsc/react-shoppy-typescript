3666659f62616c2e2c05388fd34af114e9f73b93
cho_jae_sin <1005jsc@naver.com>
2022-07-02 14:21:35 +0900

    tailwindcss settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='p-10 justify-center'>
+      <h1 className='text-9xl underline font-black text-center'>sdfsdf</h1>
+      <p className='underline '>asdfasd</p>
+      <h1 className='text-9xl underline'>sdfsdf</h1>
+      <h1 className='text-9xl underline'>sdfsdf</h1>
+      <h1 className='text-2xl underline'>sdfsdf</h1>
+      <h1 className='text-liac'>sdfsdf</h1>
     </div>
   );
 }
